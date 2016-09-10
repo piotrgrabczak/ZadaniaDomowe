46b11d644488c7805bd74303fb91848eb4b3b7ab
Anonim <piotr.grabczak@mailplus.pl>
2016-09-10 21:57:39 +0200

    Zad3

---
+++ src/Zad6_3.java
@@ -0,0 +1,28 @@
+
+public class Zad6_3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StringBuilder slowo1 = new StringBuilder("MALPKA");
+		StringBuilder slowo2 = new StringBuilder("szympans");
+		StringBuilder slowo3 = new StringBuilder();
+		
+		for (int i=0; i<slowo1.length(); i+=3)
+		{
+			slowo3.append(slowo1.substring(i,i+3)).append("");
+			if (i<=slowo2.length())
+			{
+				
+				slowo3.append(slowo2.substring(i,i+3)).append("");
+			}
+		}
+		System.out.println(slowo3+slowo2.substring(6,8));
+		
+		
+	}
+
+}
